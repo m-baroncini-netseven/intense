f4ad1552b238620359af6b4d5ab28ab73c753ecb
m-baroncini-netseven <m.baroncini@netseven.it>
2019-05-14 13:27:49 +0200

    cleaned code

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,7 +1,5 @@
 module.exports = {
   Query: {
-    letters: async (_, __, { dataSources }) =>
-      dataSources.LetterAPI.getAllLetters(),   
-    randomPerson: () =>  { return {'name':'pippo'}}
+    
   },
 };
